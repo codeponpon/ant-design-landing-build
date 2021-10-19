b9612660920a602bb9b2b016bd1545d55b138ebe
huaycoin betkub <huaycoin@betkub.com>
2021-10-19 09:22:01 +0700

    feat: added login and register components

--- src/api/deploy.js
+++ src/api/deploy.js
@@ -11,6 +11,34 @@ async function deploy(ctx, next) {
   const umiTmp = path.join(templateDir, 'umirc');
   const nowIgnoreTmp = path.join(templateDir, 'nowignore');
 
+  // Apollo Client
+  const apolloIndex = path.join(templateDir, 'apollo', 'index.js');
+
+  // Internal Library
+  const libFormatIndex = path.join(templateDir, 'libs', 'format', 'index.js');
+  const libFormatBalance = path.join(
+    templateDir,
+    'libs',
+    'format',
+    'balance.js'
+  );
+  const libAuthToken = path.join(templateDir, 'libs', 'authToken.js');
+  const libCredit = path.join(templateDir, 'libs', 'credit.js');
+  const libWaitTime = path.join(templateDir, 'libs', 'waitTime.js');
+  const libUseAuth = path.join(templateDir, 'libs', 'useAuth.js');
+
+  // Components
+  const accountComp = path.join(templateDir, 'components', 'accountComp.jsx');
+  const bankSelector = path.join(templateDir, 'components', 'bankSelector.jsx');
+  const gameListComp = path.join(templateDir, 'components', 'gameList.jsx');
+  const loginComp = path.join(templateDir, 'components', 'loginComp.jsx');
+  const providerListComp = path.join(
+    templateDir,
+    'components',
+    'providerList.jsx'
+  );
+  const registerComp = path.join(templateDir, 'components', 'registerComp.jsx');
+
   const parameter = new Parameter({
     validateRoot: true,
   });
@@ -68,24 +96,47 @@ async function deploy(ctx, next) {
   const umiContent = nunjucks.render(umiTmp);
   const nowIgnoreContent = nunjucks.render(nowIgnoreTmp);
 
+  // Apollo
+  const apolloIndexContent = nunjucks.render(apolloIndex);
+
+  // Library
+  const libFormatIndexContent = nunjucks.render(libFormatIndex);
+  const libFormatBalanceContent = nunjucks.render(libFormatBalance);
+  const libAuthTokenContent = nunjucks.render(libAuthToken);
+  const libCreditContent = nunjucks.render(libCredit);
+  const libWaitTimeContent = nunjucks.render(libWaitTime);
+  const libUseAuthContent = nunjucks.render(libUseAuth);
+
+  // Components
+  const accountCompContent = nunjucks.render(accountComp);
+  const bankSelectorContent = nunjucks.render(bankSelector);
+  const gameListCompContent = nunjucks.render(gameListComp);
+  const loginCompContent = nunjucks.render(loginComp);
+  const providerListCompContent = nunjucks.render(providerListComp);
+  const registerCompContent = nunjucks.render(registerComp);
+
   console.log(
     '---deployData.files-',
     deployData.files,
     typeof deployData.files
   );
   const solidFiles = [
-    {
-      file: 'package.json',
-      data: pkgContent,
-    },
-    {
-      file: '.umirc.js',
-      data: umiContent,
-    },
-    {
-      file: '.nowignore',
-      data: nowIgnoreContent,
-    },
+    { file: 'src/apollo/index.js', data: apolloIndexContent },
+    { file: 'src/libs/format/index.js', data: libFormatIndexContent },
+    { file: 'src/libs/format/balance.js', data: libFormatBalanceContent },
+    { file: 'src/libs/authToken.js', data: libAuthTokenContent },
+    { file: 'src/libs/credit.js', data: libCreditContent },
+    { file: 'src/libs/waitTime.js', data: libWaitTimeContent },
+    { file: 'src/libs/useAuth.js', data: libUseAuthContent },
+    { file: 'src/components/accountComp.jsx', data: accountCompContent },
+    { file: 'src/components/bankSelector.jsx', data: bankSelectorContent },
+    { file: 'src/components/gameList.jsx', data: gameListCompContent },
+    { file: 'src/components/loginComp.jsx', data: loginCompContent },
+    { file: 'src/components/providerList.jsx', data: providerListCompContent },
+    { file: 'src/components/registerComp.jsx', data: registerCompContent },
+    { file: 'package.json', data: pkgContent },
+    { file: '.umirc.js', data: umiContent },
+    { file: '.nowignore', data: nowIgnoreContent },
   ];
   const filterFiles = deployData.files.filter(
     (file) => !solidFiles.map((item) => item.file).includes(file.file)
