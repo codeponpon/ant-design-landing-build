a4c2e28c6196db9b343a8f38f56d2c01be0d02d7
cPP <pichet@like-soft.net>
2022-08-08 14:44:35 +0700

    feat: adding provider component

--- src/template/umi/components/providerList.jsx
+++ src/template/umi/components/providerList.jsx
@@ -1,40 +1,99 @@
+import React, { useEffect, useState } from 'react';
 import { Col } from 'antd';
-import { useEffect, useState } from 'react';
 import gameProviders from '../api/gameProviders';
 
-export const ProviderList = () => {
+const ProviderList = () => {
   const [providers, setProviders] = useState([]);
+  const [items, setItems] = useState([]);
+
+  const getChildrenToRender = (data) => {
+    return data.map((item) => {
+      return (
+        <Col
+          key={item.name}
+          {...item}
+          /* replace-start */
+          data-edit="Col"
+          /* replace-end */
+        >
+          <a
+            {...item.children.wrapper}
+            /* replace-start */
+            data-edit="linkA"
+            /* replace-end */
+          >
+            <span {...item.children.img}>
+              <img src={item.children.img.children} height="100%" alt="img" />
+            </span>
+            <p {...item.children.content}>
+              {
+                /* replace-start-value = item.children.content.children */
+                React.createElement('span', {
+                  dangerouslySetInnerHTML: {
+                    __html: item.children.content.children,
+                  },
+                })
+                /* replace-end-value */
+              }
+            </p>
+          </a>
+        </Col>
+      );
+    });
+  };
+
+  const getBlock = (data) => ({
+    name: data.name,
+    className: 'block',
+    md: 4,
+    xs: 24,
+    children: {
+      wrapper: {
+        className: 'content14-block-content',
+      },
+      img: {
+        children: data.img,
+      },
+      content: {
+        children: data.content,
+      },
+    },
+  });
 
   useEffect(async () => {
     if (providers.length === 0) {
+      const allProvider = [];
       const { data } = await gameProviders();
-      console.log('Providers', data);
-      setProviders(data);
+      data
+        .filter(
+          (d) =>
+            !['เกมส์ที่แจ๊คพ็อตแตกบ่อย', 'เกมส์ยอดนิยม', 'ยิงปลา'].includes(
+              d.name
+            )
+        )
+        .map((cat) => {
+          return cat.items.map((item) => {
+            allProvider[item.shortName] = item;
+            return allProvider;
+          });
+        });
+
+      setProviders(allProvider);
+
+      const blocks = Object.values(allProvider).map((item) => {
+        return getBlock({
+          name: `block${item.id}`,
+          img: item.image,
+          content: item.name,
+        });
+      });
+
+      console.log('allProvider', allProvider);
+      setItems(blocks);
     }
   }, []);
 
-  console.log('Providers', providers);
-  return providers.map((item) => {
-    return (
-      <Col
-        key={item.id}
-        md={6}
-        xs={24}
-        className="block content5-block-content"
-      >
-        <div className="title-wrapper">
-          <h2 name="title" className="title-h1 text-center">
-            <p>{item.name}</p>
-          </h2>
-          <div name="content" className="title-content">
-            <a href="#" className="content5-block-content">
-              <span>
-                <img src={item.items[0].image} height="100%" alt={item.name} />
-              </span>
-            </a>
-          </div>
-        </div>
-      </Col>
-    );
-  });
+  return getChildrenToRender(items);
 };
+
+export default ProviderList;
